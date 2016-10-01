45e9f059196068773c97bf16aadac92d37baadfd
Gancho <gancho.danailov@blue-leaf.co.uk>
2016-10-01 17:57:45 +0300

    chnage page  title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Tranc3motion' })
 })
 
 module.exports = router
--- routes/registration.js
+++ routes/registration.js
@@ -4,7 +4,7 @@ var router = express.Router()
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('registrationForm', { title: 'Express' })
+  res.render('registrationForm', { title: 'Tranc3motion' })
 })
 
 module.exports = router
