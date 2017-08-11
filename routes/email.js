dfd32968b6491555a5881c0611a8a8ff0ff39175
Gancho Danailov <gancho.danailov@gmail.com>
2017-08-11 17:35:26 +0300

    nifra changes

--- routes/email.js
+++ routes/email.js
@@ -22,8 +22,8 @@ var optionsPDF = {
 function sendTicket (cryptedString, user) {
   var qr_svg = qr.imageSync(cryptedString, { type: 'svg' })
   console.log('svgobj:' + qr_svg)
-  var date = 'May 20, 2017'
-  var artistName = 'RAM (Extended Set)'
+  var date = 'Sep 29, 2017'
+  var artistName = 'Nifra & Basil O\'Glue'
   var clubName = 'Fabrika 126'
   var guestEmail = user.email
   var name = user.firstName + ' ' + user.lastName
